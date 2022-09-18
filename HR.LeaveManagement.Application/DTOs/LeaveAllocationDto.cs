3d2d520a7a336147c26d1787bc212cd291d5a044
wendelsimoes <wendel.simoes.br@gmail.com>
2022-09-18 15:01:56 -0300

    Implementing Automapper.

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
